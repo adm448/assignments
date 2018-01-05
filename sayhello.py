c28f8c423f545943376cbe98abc00a2535310267
Adam Murray <adm448@student.hks.harvard.edu>
2018-01-05 16:21:05 -0500

    add comment to sayhello.py

--- sayhello.py
+++ sayhello.py
@@ -1,4 +1,7 @@
 #!/usr/bin/env python2
+#This is a simple python program.
+#It takes a simple input and prints it to the terminal with a greeting.
+
 import sys
 name = sys.stdin.read()
 print "Hello " + name +"!"
