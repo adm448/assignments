a4de34b27fe31d94d465af3e76e6db25193c9a76
Adam Murray <adm448@student.hks.harvard.edu>
2018-01-07 16:19:01 -0500

    test functions

--- calculator.js
+++ calculator.js
@@ -16,3 +16,8 @@ function divide(a,b) {
 console.log("I'm going to use the calculator to divie 10 by 5.")
 var x = divide(10,5)
 console.log(x)
+
+//test
+console.log(add(1,2))
+console.log(subtract(10,2))
+console.log(multiply(3,4))
